714e1dc8e98c9f56cf332362cbed58f0b066956a
unknown <eduard.tudorache@gmail.com>
2024-07-01 16:26:16 +0300

    eu

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const headerA1 = document.querySelector(".headerA1")
 const headerA2 = document.querySelector(".headerA2")
 const headerA3 = document.querySelector(".headerA3")
 const headerA4 = document.querySelector(".headerA4")
-
+const hamburgerA = document.querySelector(".hamburger") 
+const bar = document.querySelector(".bar")
+const bar1 = document.querySelector(".bar1")
+const bar2 = document.querySelector(".bar2")
 
 window.onscroll = function(){
     var top = window.scrollY;
@@ -15,6 +18,11 @@ window.onscroll = function(){
         headerA2.classList.add("activeA")
         headerA3.classList.add("activeA")
         headerA4.classList.add("activeA")
+        hamburgerA.classList.add("activeH")
+        bar.classList.add("activeH")
+        bar1.classList.add("activeH")
+        bar2.classList.add("activeH")
+
     }else{
         header.classList.remove("active")
         headerA.classList.remove("activeA")
@@ -22,6 +30,18 @@ window.onscroll = function(){
         headerA2.classList.remove("activeA")
         headerA3.classList.remove("activeA")
         headerA4.classList.remove("activeA")
-    }
-    
+        hamburgerA.classList.remove("activeH")
+        bar.classList.remove("activeH")
+        bar1.classList.remove("activeH")
+        bar2.classList.remove("activeH")
+
+    } 
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
